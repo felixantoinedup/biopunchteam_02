5e55fffaa560ec054b163a965a1466f987cf09a7
Unknown <emaines@gmail.com>
2018-11-24 13:11:44 -0500

    Mostly Pollen system, with unique plant detect, reduce plant life over time, but Delete flower and spawn new flower TODO

---
+++ BioPunchUnity/Assets/Scripts/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    List<Plant> plants;
+
+	// Use this for initialization
+	void Start () {
+        plants = new List<Plant>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
