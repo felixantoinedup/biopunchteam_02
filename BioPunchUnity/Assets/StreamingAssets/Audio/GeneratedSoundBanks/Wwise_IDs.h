168e5cfff1f9c2867730e7d448f16cfef042be81
Unknown <jean.gravel2@yahoo.com>
2018-11-24 19:28:27 -0500

    Wwise - 1st batch
    Integre: Music, ambiance, bee % flower SFX

--- BioPunchUnity/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ BioPunchUnity/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,10 +13,45 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID COLOURSHIFT = 3228142023U;
-        static const AkUniqueID FOOTSTEPS = 2385628198U;
-        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID AMB_LEVEL_START = 378653913U;
+        static const AkUniqueID MUS_LAYER2 = 445604206U;
+        static const AkUniqueID MUS_LAYER3 = 445604207U;
+        static const AkUniqueID MUSIC_START = 3725903807U;
+        static const AkUniqueID SFX_BEE_BUZZ = 664649349U;
+        static const AkUniqueID SFX_BEE_TWERK = 1416159775U;
+        static const AkUniqueID SFX_CROSSBREED_SUCCESS = 3242134711U;
+        static const AkUniqueID SFX_FLOWER_BREED1 = 2001778080U;
+        static const AkUniqueID SFX_FLOWER_BREED2 = 2001778083U;
+        static const AkUniqueID SFX_FLOWER_BREED3 = 2001778082U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAME_INTENSITY
+        {
+            static const AkUniqueID GROUP = 3469768597U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LAYER1 = 3298531297U;
+                static const AkUniqueID LAYER2 = 3298531298U;
+                static const AkUniqueID LAYER3 = 3298531299U;
+            } // namespace STATE
+        } // namespace GAME_INTENSITY
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID FLY_SPEED = 3034937204U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID BREED1 = 789210894U;
+        static const AkUniqueID BREED2 = 789210893U;
+        static const AkUniqueID BREED3 = 789210892U;
+    } // namespace TRIGGERS
 
     namespace BANKS
     {
@@ -26,6 +61,8 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID BEE_BUZZ = 1461531525U;
+        static const AkUniqueID BEE_TWERK = 437129503U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
     } // namespace BUSSES
 
